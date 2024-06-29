8b5db236050c7938147f0899d2590f376a717914
1986skh <khelifa.smail08@gmail.com>
2024-06-29 17:26:07 +0200

    second commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,18 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
   e2e: {
     baseUrl: "https://practice.expandtesting.com/notes/api",
     pageLoadTimeout: 60000,
     defaultCommandTimeout: 10000,
     setupNodeEvents(on, config) {
-      // Ajouter des écouteurs d'événements ou d'autres configurations si nécessaire
-     
-    }
-  }
+      require("cypress-mochawesome-reporter/plugin")(on);
+      
+      // Ajouter d'autres écouteurs d'événements ou configurations si nécessaire
+    },
+  },
 });
+
 
 
